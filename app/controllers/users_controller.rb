63c653f4c3c578a1b73e4b2d0e166a288f16f621
ilia <iliavabez@gmail.com>
2018-04-19 14:55:21 +0300

    initial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  #этот контроллер будет создавать, авторизовывать пользователей и что-нибудь еще
+
+  def show
+    @hello = "Шалом, брат!"
+  end
+end
